e0fa771e5f1ac393d33b87e962acdc6c592c0b32
rajesh30062003 <rjrajeshpal30@gmail.com>
2026-06-15 18:11:34 +0530

    Fix TypeScript build for Render

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -8,7 +8,9 @@ export class AppError extends Error {
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
+    if ((Error as any).captureStackTrace) {
+      (Error as any).captureStackTrace(this, this.constructor);
+    }
   }
 }
 
